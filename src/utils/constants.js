65fcfafb1e49a9013b01cc72c3cffe400cac1ce5
ArkaPrabhaChowdhury <arkopra@gmail.com>
2023-02-28 00:21:50 +0530

    changes

---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+import CodeIcon from '@mui/icons-material/Code';
+import ComputerIcon from '@mui/icons-material/Computer';
+import LanguageIcon from '@mui/icons-material/Language';
+import CalculateIcon from '@mui/icons-material/Calculate';
+import SchoolIcon from '@mui/icons-material/School';
+import ScienceIcon from '@mui/icons-material/Science';
+import BiotechIcon from '@mui/icons-material/Biotech';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import FollowTheSignsIcon from '@mui/icons-material/FollowTheSigns';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'English', icon: <LanguageIcon />, },
+  {name:'Computer Science',icon:<ComputerIcon/>},
+  { name: 'Math', icon: <CalculateIcon />, },
+  { name: 'Chemistry', icon: <ScienceIcon />, },
+  { name: 'Biology', icon: <BiotechIcon/>, },
+  { name: 'Commerce', icon: <MonetizationOnIcon/>, },
+  { name: 'Motivation', icon: <FollowTheSignsIcon/>, },
+  
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
